0471fd96eb33c4b19549d4a39901d48e7980e990
venkatbhattiprolu <vbhat847@gmail.com>
2023-10-01 22:53:43 -0700

    migrating to terraform cloud

---
+++ providers.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.19.0"
+    }
+  }
+}
+
+provider "random" {
+  # Configuration options
+}
